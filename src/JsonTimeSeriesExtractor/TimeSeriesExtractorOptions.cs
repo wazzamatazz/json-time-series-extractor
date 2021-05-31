a6d9bab6927cdbaee5c1d2e92855a84d9528e6e1
Graham Watts <grahamcharleswatts@gmail.com>
2021-05-31 13:57:14 +0300

    Update documentation

--- src/JsonTimeSeriesExtractor/TimeSeriesExtractorOptions.cs
+++ src/JsonTimeSeriesExtractor/TimeSeriesExtractorOptions.cs
@@ -39,8 +39,13 @@ namespace Jaahas.Json {
         /// </para>
         /// 
         /// <para>
-        ///   Use the <see cref="ExcludeProperties"/> property to ignore JSON properties that are 
+        ///   Use the <see cref="IncludeProperty"/> delegate to ignore JSON properties that are 
         ///   not required or are used only for metadata purposes.
+        /// </para>
+        /// 
+        /// <para>
+        ///   Important: When <see cref="Recursive"/> mode is enabled, placeholders behave differently. 
+        ///   See the documentation for the <see cref="Recursive"/> property for more information.
         /// </para>
         /// 
         /// </remarks>
@@ -109,7 +114,9 @@ namespace Jaahas.Json {
         ///   The parameter passed to the delegate is an array of <see cref="KeyValuePair{String, JsonElement}"/> 
         ///   that represents the JSON property names and elements that have been visited to reach 
         ///   the property that is currently being processed, with the entry for the current 
-        ///   property first on the list, and the entry for the property on the root object last.
+        ///   property first on the list, and the entry for the property on the root object last. 
+        ///   The entry for the root object in the document will always have a <see langword="null"/> 
+        ///   <see cref="KeyValuePair{String, JsonElement}.Key"/> value.
         /// </para>
         /// 
         /// <para>
@@ -147,8 +154,8 @@ namespace Jaahas.Json {
         /// </code>
         /// 
         /// <para>
-        ///   Given a key template of <c>devices/{deviceId}/{$prop}</c>, <see cref="ExcludeProperties"/> 
-        ///   configured to include <c>deviceId</c>, recursive processing enabled, and a path 
+        ///   Given a key template of <c>devices/{deviceId}/{$prop}</c>, <see cref="IncludeProperty"/> 
+        ///   configured to skip <c>deviceId</c>, recursive processing enabled, and a path 
         ///   separator of <c>/</c>, values for the following keys will be emitted:
         /// </para>
         /// 
@@ -187,6 +194,33 @@ namespace Jaahas.Json {
         ///   </item>
         ///   <item>
         ///     <description><c>devices/1/measurements/1</c></description>
+        ///   </item>
+        /// </list>
+        /// 
+        /// <para>
+        ///   Note that, when running in recursive mode, template placeholder replacements behave 
+        ///   differently:
+        /// </para>
+        /// 
+        /// <list type="bullet">
+        ///   <item>
+        ///     <description>
+        ///       When a template includes a reference to another property (e.g. <c>deviceId</c> in <c>{deviceId}/{$prop}</c>), 
+        ///       all instances of that property from the root object to the current element will 
+        ///       be used in the replacement, using the <see cref="PathSeparator"/> to join them 
+        ///       together. For example, if a parent object has a <c>deviceId</c> of <c>1</c> and 
+        ///       a child object has a <c>deviceId</c> of <c>A</c>, the <c>{deviceId}</c> placeholder 
+        ///       in the key template will be replaced with <c>1/A</c> when it is applied to a 
+        ///       property on the child object.
+        ///     </description>
+        ///   </item>
+        ///   <item>
+        ///     <description>
+        ///       The <c>{$prop}</c> placeholder is replaced with the names of every property that 
+        ///       was visited in order to arrive at the current property from the root object (e.g. 
+        ///       <c>measurements/acceleration/X</c>). If you only require the local, unqualified 
+        ///       property name in your generated keys, you can use the <c>{$prop-local}</c> instead.
+        ///     </description>
         ///   </item>
         /// </list>
         /// 
