54902055743f02fff1a6956f5f2febbd28235811
Graham Watts <grahamcharleswatts@gmail.com>
2024-04-05 14:45:30 +0300

    Add sample program

---
+++ samples/JsonTimeSeriesExtractor.Cli/Program.cs
@@ -0,0 +1,31 @@
+﻿using System.Text.Json;
+using System.Text.Json.Serialization;
+
+using Jaahas.Json;
+
+using Json.Pointer;
+
+var jsonOptions = new JsonSerializerOptions(JsonSerializerDefaults.Web);
+jsonOptions.Converters.Add(new JsonStringEnumConverter());
+
+JsonElement json;
+using (var stream = new FileStream("data.json", FileMode.Open)) {
+    json = await JsonSerializer.DeserializeAsync<JsonElement>(stream, jsonOptions);
+}
+
+var options = new TimeSeriesExtractorOptions() { 
+    AllowNestedTimestamps = true,
+    IncludeArrayIndexesInSampleKeys = false,
+    IncludeProperty = p => !p.Segments.Last().Value.Equals("t"),
+    MaxDepth = 5,
+    Recursive = true,
+    StartAt = JsonPointer.Parse("/body/data"),
+    TimestampProperty = JsonPointer.Parse("/ts"),
+    Template = "{t}"
+};
+
+
+
+foreach (var sample in TimeSeriesExtractor.GetSamples(json, options)) {
+    Console.WriteLine(JsonSerializer.Serialize(sample, jsonOptions));
+}
