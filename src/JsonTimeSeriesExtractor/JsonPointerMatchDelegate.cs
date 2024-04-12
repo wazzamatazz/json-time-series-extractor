ebc9a192fe09dbd225927254b3d873a9cb2ea819
Graham Watts <grahamcharleswatts@gmail.com>
2024-04-12 15:52:42 +0300

    Add JsonPointerMatchDelegate
    `JsonPointerMatchDelegate` defines a delegate signature that is used when selecting elements to process in a JSON document.

---
+++ src/JsonTimeSeriesExtractor/JsonPointerMatchDelegate.cs
@@ -0,0 +1,25 @@
+﻿using System.Text.Json;
+
+using Json.Pointer;
+
+namespace Jaahas.Json {
+
+    /// <summary>
+    /// A delegate that can be used to test if <see cref="TimeSeriesExtractor"/> should process a 
+    /// JSON element.
+    /// </summary>
+    /// <param name="context">
+    ///   The time series extractor context.
+    /// </param>
+    /// <param name="pointer">
+    ///   The JSON pointer to the element.
+    /// </param>
+    /// <param name="element">
+    ///   The JSON element.
+    /// </param>
+    /// <returns>
+    ///   <see langword="true"/> if the element should be processed; otherwise, <see langword="false"/>.
+    /// </returns>
+    public delegate bool JsonPointerMatchDelegate(TimeSeriesExtractorContext context, JsonPointer pointer, JsonElement element);
+
+}
