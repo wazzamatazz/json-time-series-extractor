e8d631de5334b49d9f7273125348c148e8aeb58f
Graham Watts <grahamcharleswatts@gmail.com>
2024-04-16 09:40:02 +0300

    Update example app to demonstrate two data formats
    Updates the example app to show how two different JSON formats containing the same data samples can be processed.

--- samples/JsonTimeSeriesExtractor.Cli/Program.cs
+++ samples/JsonTimeSeriesExtractor.Cli/Program.cs
@@ -7,11 +7,12 @@ var jsonOptions = new JsonSerializerOptions(JsonSerializerDefaults.Web);
 jsonOptions.Converters.Add(new JsonStringEnumConverter());
 
 JsonElement json;
-using (var stream = new FileStream("data.json", FileMode.Open)) {
+
+using (var stream = new FileStream("data-1.json", FileMode.Open)) {
     json = await JsonSerializer.DeserializeAsync<JsonElement>(stream, jsonOptions);
 }
 
-var options = new TimeSeriesExtractorOptions() {
+var options1 = new TimeSeriesExtractorOptions() {
     // We want to extract samples from nested objects.
     Recursive = true,
     // Our JSON contains an array of samples, each with its own timestamp.
@@ -21,7 +22,7 @@ var options = new TimeSeriesExtractorOptions() {
     // Each object in the samples array contains a "v" property with the sample value. We'll use
     // an MQTT-style match expression to extract values from the "v" property on each object in
     // the array
-    CanProcessElement = TimeSeriesExtractor.CreateJsonPointerMatchDelegate(new JsonPointerMatchDelegateOptions() { 
+    CanProcessElement = TimeSeriesExtractor.CreateJsonPointerMatchDelegate(new JsonPointerMatchDelegateOptions() {
         PointersToInclude = ["/body/data/+/v"],
         AllowWildcardExpressions = true
     }),
@@ -30,6 +31,39 @@ var options = new TimeSeriesExtractorOptions() {
     Template = "{t}"
 };
 
-foreach (var sample in TimeSeriesExtractor.GetSamples(json, options)) {
+Console.WriteLine();
+Console.WriteLine("Extracted samples (data-1.json):");
+Console.WriteLine();
+foreach (var sample in TimeSeriesExtractor.GetSamples(json, options1)) {
     Console.WriteLine(JsonSerializer.Serialize(sample, jsonOptions));
 }
+
+
+using (var stream = new FileStream("data-2.json", FileMode.Open)) {
+    json = await JsonSerializer.DeserializeAsync<JsonElement>(stream, jsonOptions);
+}
+
+var options2 = new TimeSeriesExtractorOptions() {
+    // We want to extract samples from nested objects.
+    Recursive = true,
+    // Our JSON contains an array of samples, each with its own timestamp.
+    AllowNestedTimestamps = true,
+    // The timestamp is located at the "ts" property on each sample object.
+    TimestampProperty = "/ts",
+    // Each object in the samples array contains a "v" property with the sample value. We'll use
+    // an MQTT-style match expression to extract values from the "v" property on each object in
+    // the array
+    CanProcessElement = TimeSeriesExtractor.CreateJsonPointerMatchDelegate(new JsonPointerMatchDelegateOptions() {
+        PointersToInclude = ["/body/data/+/v", "/body/data/+/+/v"],
+        AllowWildcardExpressions = true
+    }),
+    IncludeArrayIndexesInSampleKeys = false
+};
+
+Console.WriteLine();
+Console.WriteLine("Extracted samples (data-2.json):");
+Console.WriteLine();
+foreach (var sample in TimeSeriesExtractor.GetSamples(json, options2)) {
+    Console.WriteLine(JsonSerializer.Serialize(sample, jsonOptions));
+}
+
