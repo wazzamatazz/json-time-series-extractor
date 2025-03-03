90391a05c5ab26821fcdc4bef759de219f5d7761
Graham Watts <grahamcharleswatts@gmail.com>
2025-03-03 20:22:32 +0200

    Add fixed-length stack types
    Adds new fixed-length stack types that use ArrayPool<T> to obtain a backing buffer. The intention here is to reduce the number of allocations required when repeatedly processing JSON payloads.

---
+++ src/JsonTimeSeriesExtractor/TimestampStack.cs
@@ -0,0 +1,11 @@
+﻿namespace Jaahas.Json;
+
+/// <summary>
+/// A stack of <see cref="ParsedTimestamp"/> items.
+/// </summary>
+internal sealed class TimestampStack : FixedLengthStack<ParsedTimestamp> {
+
+    /// <inheritdoc />
+    public TimestampStack(int capacity) : base(capacity) { }
+
+}
