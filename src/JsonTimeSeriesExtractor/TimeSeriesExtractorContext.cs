73254eb46a760afa5b1ad6e94b46301276f4ba94
Graham Watts <grahamcharleswatts@gmail.com>
2025-03-03 20:57:43 +0200

    Fix stack size in non-recursive mode

--- src/JsonTimeSeriesExtractor/TimeSeriesExtractorContext.cs
+++ src/JsonTimeSeriesExtractor/TimeSeriesExtractorContext.cs
@@ -62,7 +62,8 @@ namespace Jaahas.Json {
                     : options.MaxDepth
                 : 1;
             
-            ElementStack = new ElementStack(MaxDepth);
+            // We need to add 1 to the maximum depth to allow for the root element.
+            ElementStack = new ElementStack(MaxDepth + 1);
             TimestampStack = new TimestampStack(options.Recursive && options.AllowNestedTimestamps ? MaxDepth : 1);
 
             // We are using the default sample key template if:
